e14a6d45afc29fbc6ab2d5e241a77e0c9e701824
Jonah Snider <jonah@jonah.pw>
2020-01-30 23:21:03 -0800

    perf(css): only import required CSS
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,36 @@
-console.log("bog");
+import "regenerator-runtime/runtime";
+import {
+    register,
+} from "./api";
+
+/**
+ * The form element where the user submits their email to be registered.
+ *
+ * @type {HTMLFormElement}
+ */
+const form = document.getElementById("form");
+/**
+ * The email input field.
+ *
+ * @type {HTMLInputElement}
+ */
+const emailField = document.getElementById("email-field");
+
+const submitButton = document.getElementById("submit-button");
+
+form.addEventListener("submit", async event => {
+    event.preventDefault();
+
+    const { value: email } = emailField;
+
+    try {
+        submitButton.classList.add("is-loading");
+        await register(email);
+    }
+    catch (fetchError) {
+        console.error(fetchError);
+    }
+    finally {
+        submitButton.classList.remove("is-loading");
+    }
+});
